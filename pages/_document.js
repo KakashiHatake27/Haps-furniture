49cc92a6805e7609c8f6083ae2089425fba4ca72
KakashiHatake27 <trialacc.akshdeep@gmail.com>
2023-07-21 01:50:10 -0400

    added meta data

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href="/favicon-logo.ico"/>
+        <meta name='description' content="Explore a vast selection of stylish and affordable furniture for every room. Discover top-notch sofas, beds, and dining sets to suit your unique taste. Great deals available - Shop now!"/>
         </Head>
       <body>
         <Main />
