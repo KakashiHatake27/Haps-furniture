8e5370c91cfad5dd156b01d8312350b8414242dc
KakashiHatake27 <trialacc.akshdeep@gmail.com>
2023-07-21 00:16:00 -0400

    Added functionality for social media , added logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillFacebook, AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
 <p>&copy; 2023 HAPS Furniture All rights reserved</p>
 <p className='icons'>
-  <AiFillInstagram/>
-  <AiOutlineTwitter/>
+
+<Link target="_blank" href="https://www.instagram.com/haps.furniture/"><AiFillInstagram/></Link>
+
+{/* <button type="button" className='ig-icon' onClick={"_https://www.instagram.com/haps.furniture/"}><AiFillInstagram/></button> */}
+  <Link target="_blank" href="https://www.facebook.com/people/Haps-Furniture/100094399224312/"><AiFillFacebook/></Link>
+  {/* <AiFillInstagram href='https://www.instagram.com/haps.furniture/'/> */}
+  {/* <AiOutlineTwitter/> */}
 </p>
     </div>
   )
